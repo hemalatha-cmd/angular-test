eaaef2174c1af33fa5b3ed107988c8ec67fb239e
Hemalatha <hemalatha@test.com>
2020-11-05 06:32:09 +0530

    first commit

---
+++ src/app/_services/account.service.ts
@@ -0,0 +1,67 @@
+﻿import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AccountService {
+    private userSubject: BehaviorSubject<User>;
+    public user: Observable<User>;
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) {
+        if(localStorage.getItem('user') !=null) {
+        this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user'))[0]);
+        this.user = this.userSubject.asObservable();
+        }else{
+            this.userSubject = new BehaviorSubject<User>(new User());
+            this.user = this.userSubject.asObservable();
+        }
+    }
+
+    public get userValue(): User {
+        return this.userSubject.value;
+    }
+
+    login(username) {
+        return this.http.get<User[]>(`${environment.apiUrl}users?email=`+username)
+            .pipe(map(user => {
+
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('user', JSON.stringify(user));
+                if(user.length >0)
+                this.userSubject.next(user[0]);
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('user');
+        this.userSubject.next(null);
+        this.router.navigate(['/account/login']);
+    }
+
+
+    getPostsByUser(userId) {
+        return this.http.get<User[]>(`${environment.apiUrl}posts?userId=`+userId);
+    }
+
+    getCommentsByPostId(postId) {
+        return this.http.get<User[]>(`${environment.apiUrl}comments?postId=`+postId );
+    }
+
+    getById(id: string) {
+        return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+    }
+
+    
+
+  
+}
---
+++ src/app/users/list.component.ts
@@ -0,0 +1,37 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { AccountService } from '@app/_services';
+
+@Component({ templateUrl: 'list.component.html' })
+export class ListComponent implements OnInit {
+    posts = null;
+    user = null;
+
+    constructor(private accountService: AccountService) {}
+
+    ngOnInit() {
+        this.user = this.accountService.userValue;
+        this.accountService.getPostsByUser(this.user.id)
+            .pipe(first())
+            .subscribe(posts =>{ 
+                this.posts = posts
+                this.posts.forEach(post => {
+                    console.log(post);
+                    this.accountService.getCommentsByPostId(post.id).subscribe(commets =>{
+                        post.comments = commets;
+                    })
+                });
+                
+
+            });
+         
+            
+    }
+
+    getCommentsByPostId(postId){
+        return this.accountService.getCommentsByPostId(postId);
+    }
+
+ 
+}
